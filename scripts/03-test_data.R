eaa199e19a0b14536d2b71450dce51e7f1e75892
Thu Dong <thu.dong@mail.utoronto.ca>
2024-01-25 16:42:32 +0000

    last commit

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,17 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests
+# Author: Thu Dong
+# Date: 24 Jan 2024
+# Contact:thu.dong@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites:run download data and test data 
+
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Test data ####
+clean_data$TOTAL_CRIME_2023|> min() >= 4
+clean_data$POPULATION_2023 |> max() <= 37000
+clean_data$POPULATION_2023 |> class() == "numeric"
